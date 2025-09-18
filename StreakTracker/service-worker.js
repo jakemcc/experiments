744019cb241969c424ff54e23ee7dd4afe74e3f0
Jake McCrary <jake@jakemccrary.com>
2025-09-17 19:37:30 -0500

    Remove PNG icons and update PWA metadata

---
+++ StreakTracker/service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'streak-tracker-cache-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './dist/main.js',
+  './manifest.webmanifest',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+      )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  const requestURL = new URL(request.url);
+  if (requestURL.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(request)
+        .then((networkResponse) => {
+          if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+            const responseClone = networkResponse.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, responseClone));
+          }
+          return networkResponse;
+        })
+        .catch((error) => {
+          if (request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+          throw error;
+        });
+    })
+  );
+});
